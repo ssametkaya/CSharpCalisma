a0d7df4c3dcc7a6a63c0c893f85926b2e0d751aa
ssametkaya <ssametkaya52@gmail.com>
2022-03-03 22:20:10 +0300

    arrays

---
+++ CsharpCourse/Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students1 = new string[4];
+            students1[0] = "samet";
+            students1[1] = "ahmet";
+            students1[2] = "mehmet";
+            students1[3] = "anıl";
+
+            //string[] students = new[] { "anıl", "ahmet", "mehmet", "samaet" };
+
+            string[] students = { "anıl", "ahmet", "memet", "sameteee" };
+            
+            foreach (var student in students1)
+            {
+                Console.WriteLine(student);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsharpCourse/MethodOrnekler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOrnekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Bir Sayıyı Giriniz:");
+
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //tekcift(sayi);
+            //Console.ReadKey();
+            
+            Console.WriteLine(Add(6,5,4,3,2,1));
+            Console.ReadLine();
+           
+        }
+        static int Add(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+
+
+        //static void tekcift (int a)
+        //{
+            //if (a % 2 ==0 )
+            //{
+                //Console.WriteLine("Sayı Cift");
+            //}
+
+            //else
+            //{
+                //Console.WriteLine("Sayı Tek");
+            //}
+        //}
+    }
+}
