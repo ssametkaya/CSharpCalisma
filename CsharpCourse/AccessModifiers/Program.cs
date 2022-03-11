da2226fdeee8d1180ac8e4156f18b99f9bf2dba0
ssametkaya <ssametkaya52@gmail.com>
2022-03-11 22:44:09 +0300

    Constructors

---
+++ CsharpCourse/AccessModifiers/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        protected int Id { get; set; } // protected kalıtım ile bağlı olan classlarda da kullanılabilir
+        private int Value { get; set; } // private tanımlama sadece tanımlanan classda kullanılır
+
+        public void Savee()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+    }
+
+    class Student:Customer
+    {
+        public void Save()
+        {
+           
+        }
+    }
+
+    internal class Course  // Olusturulan classların default erişimi internaldır.
+    {
+        public string Name { get; set; }
+
+        private class NestedClass  //class için private ve protected erişim bildirgiçleri kullanılamaz -- sadece iç içe classlarda kullanılabilir.
+        {
+
+        }
+    }
+
+    public class Course2 // public kullanarak oluşturulan classlar ise başka projelerde de kullanılabilir
+    {
+
+    }
+
+    
+}
---
+++ CsharpCourse/Constructors/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(10);
+
+            customerManager.List();
+
+            Product product = new Product(2, "Computer");
+
+
+            EmployeeManager employeeManager = new EmployeeManager(new FileLogger());
+            employeeManager.Add();
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class CustomerManager
+    {
+        private int _count;
+        public CustomerManager(int count)
+        {
+            _count = count;
+        }
+        public void List()
+        {
+            Console.WriteLine("Listed! {0} items", _count);
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Added!");
+        }
+    }
+
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        private int _id;
+        private string _name;
+
+        public Product(int id,string name)
+        {
+            _id = id;
+            _name = name;
+
+        }
+
+    }
+
+    class EmployeeManager
+    {
+        private ILogger _logger;
+        public EmployeeManager(ILogger logger)
+        {
+            _logger = logger;
+
+        }
+
+        public void Add()
+        {
+            _logger.Log();
+            Console.WriteLine("Added!");
+        }
+    }
+
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to File");
+        }
+    }
+
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to sms");
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+}
