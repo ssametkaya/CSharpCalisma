ee655e2f0aa485c92bb5e1b379e132b433431b1d
ssametkaya <ssametkaya52@gmail.com>
2022-03-12 22:47:43 +0300

    Reference and Valute Type

---
+++ CsharpCourse/ReferenceAndValuType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceAndValuType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
+
+            Console.WriteLine(number2);
+
+
+            string[] cities1 = new string[] { "ordu", "van", "ısparta" };
+            string[] cities2 = new string[] { "istanbul", "bursa", "tekirdağ" };
+
+            cities2 = cities1;
+            cities1[0] = "ankara";
+
+            Console.WriteLine(cities2[0]);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
