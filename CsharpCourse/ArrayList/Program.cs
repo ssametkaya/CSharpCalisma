8893d2b570a154336ed502d2f4bbaa28759ab302
ssametkaya <ssametkaya52@gmail.com>
2022-03-13 22:40:34 +0300

    Collections

---
+++ CsharpCourse/ArrayList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList tekSayilar = new ArrayList();
+            //ArrayList ciftSayilar = new ArrayList();
+            //int ortalama = 0, toplam = 0;
+            //Random rnd = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int sayi = rnd.Next(1, 100);
+            //    Console.Write(sayi + "   ");
+            //    toplam += sayi;
+            //    if (sayi % 2 == 0)
+            //    {
+            //        ciftSayilar.Add(sayi);
+            //    }
+            //    else
+            //    {
+            //        tekSayilar.Add(sayi);
+            //    }
+
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("=====");
+            //ortalama = toplam / 10;
+
+            //Console.WriteLine("Sayıların Ortalaması : {0}", ortalama);
+            //Console.WriteLine("Tek Sayıların Adeti : {0}", tekSayilar.Count);
+            //Console.WriteLine("Çift Sayıların Adeti : {0}", ciftSayilar.Count);
+            //Console.ReadKey();
+
+            List<int> sayilar = new List<int>();
+            for (int i = 1; i <= 49; i++)
+            {
+                sayilar.Add(i);
+            }
+            for (int x = 1; x <= 6; x++)
+            {
+                System.Threading.Thread.Sleep(1000); 
+                Random rnd = new Random();
+                int index = rnd.Next(0, sayilar.Count);
+                Console.WriteLine(sayilar[index]);
+                sayilar.RemoveAt(index);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsharpCourse/Collectinos/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collectinos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] cities = new string[2] { "Isparta", "Van" };
+
+            //cities = new string[3];
+            //Console.WriteLine(cities[1]);
+
+            //ArrayList cities = new ArrayList();
+            //cities.Add("Ankara");
+            //cities.Add("Ordu");
+            //cities.Add(15);
+            //cities.Add("Van");
+            //cities.Add("a");
+            //cities.Add(1515.5);
+
+            //foreach (var city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+
+            //List<string> cities = new List<string>(); // Listler ile tip bazlı çalışılır belirlenen değişken türü dışında değişken eklenemez.
+
+            //cities.Add("Ankara");
+            //cities.Add("Van");
+            //cities.Add(15);
+            //foreach (var city in cities)
+            //{
+            //    Console.WriteLine(city);
+            //}
+
+
+
+
+            //List<Customer> customers = new List<Customer>();
+            //customers.Add(new Customer { FirstName = "samet", Id = 123 });
+            //customers.Add(new Customer { FirstName = "ahmet", Id = 123123 });
+
+            List<Customer> customers = new List<Customer>
+            {
+
+                new Customer { FirstName = "yigit", Id = 23232 },
+
+                new Customer { FirstName = "dogukan", Id = 5656 }
+             };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+    }
+}
+
+
